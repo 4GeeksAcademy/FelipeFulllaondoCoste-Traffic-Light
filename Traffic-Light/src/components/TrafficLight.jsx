5bf68bb399da78c06af6bdc6e1a65c2f0e242411
Felipe Coste <32721311+felipecoste@users.noreply.github.com>
2024-10-23 22:56:23 +0000

    commit

--- Traffic-Light/src/components/TrafficLight.jsx
+++ Traffic-Light/src/components/TrafficLight.jsx
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import '/src/App.css'
 
 const TrafficLight = () => {
 
+    const [selected, setSelected] = useState(null)
 
-    return(
-        <h1>hola</h1>
+    const prenderLuz = (id) => {
+        setSelected(id);
+    }
+
+
+    return (
+        <>
+            <div
+                className="container d-flex flex-column align-items-center bg-dark p-3"
+                style={{ width: "20px", height: "150px" }}>
+            </div>
+
+            <div className="container d-flex flex-column align-items-center bg-dark p-3 " style={{ width: "150px" }}>
+                <div
+                    className={`bg-danger rounded-circle ${selected === 'red' ? 'glow' : ''}`}
+                    style={{ width: "100px", height: "100px" }}
+                    onClick={() => prenderLuz('red')}>
+                </div>
+                <div
+                    className={`bg-warning rounded-circle ${selected === 'yellow' ? 'glow' : ''}`}
+                    style={{ width: "100px", height: "100px" }}
+                    onClick={() => prenderLuz('yellow')}>
+                </div>
+                <div
+                    className={`bg-success rounded-circle ${selected === 'green' ? 'glow' : ''}`}
+                    style={{ width: "100px", height: "100px" }}
+                    onClick={() => prenderLuz('green')}>
+                </div>
+            </div>
+        </>
     );
 };
 
