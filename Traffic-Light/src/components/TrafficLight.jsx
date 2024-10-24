28af3daf80dc3c94d059d53c3038882e9778cf82
Felipe Coste <32721311+felipecoste@users.noreply.github.com>
2024-10-24 00:08:49 +0000

    Se agrega color violet

--- Traffic-Light/src/components/TrafficLight.jsx
+++ Traffic-Light/src/components/TrafficLight.jsx
@@ -4,6 +4,7 @@ import '/src/App.css'
 const TrafficLight = () => {
 
     const [selected, setSelected] = useState(null)
+    const [showViolet, setShowViolet] = useState(false)
 
     const prenderLuz = (id) => {
         setSelected(id);
@@ -19,6 +20,11 @@ const TrafficLight = () => {
         }
     }
 
+    const colorExtra = () => {
+        setShowViolet(!showViolet)
+    }
+
+
     return (
         <>
             <div
@@ -26,7 +32,7 @@ const TrafficLight = () => {
                 style={{ width: "20px", height: "150px" }}>
             </div>
 
-            <div className="container d-flex flex-column align-items-center bg-dark p-3 rounded" style={{ width: "150px" }}>
+            <div className="container d-flex flex-column align-items-center bg-dark p-3 rounded mb-3" style={{ width: "150px" }}>
                 <div
                     className={`bg-danger rounded-circle ${selected === 'red' ? 'glow' : ''}`}
                     style={{ width: "100px", height: "100px" }}
@@ -42,11 +48,25 @@ const TrafficLight = () => {
                     style={{ width: "100px", height: "100px" }}
                     onClick={() => prenderLuz('green')}>
                 </div>
+                
+                
+                {showViolet && (
+                    <div
+                        className={`bg-violet rounded-circle ${selected === 'violet' ? 'glow' : ''}`}
+                        style={{ width: "100px", height: "100px" }}
+                        onClick={() => prenderLuz('violet')}>
+                    </div>
+                )}
+
             </div>
 
 
             <div className="container d-flex flex-column align-items-center p-3">
                 <button onClick={alternarLuz} type="button" className="btn btn-outline-light btn-lg border">Alternar Color</button>
+            </div>
+
+            <div className="container d-flex flex-column align-items-center p-3">
+                <button onClick={colorExtra} type="button" className="btn btn-outline-light btn-lg border">Color Extra</button>
             </div>
         </>
     );
